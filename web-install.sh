b2eb42e793b1e73f9bbe15ce9e4bc3fec8ce72ac
root <root@localhost.localdomain>
2019-03-24 01:26:24 +0200

    Web installer

---
+++ web-install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+#!/bin/bash
+
+if [ $EUID -ne 0 ]; then
+	echo "Run install.sh with root!"
+	exit 1;
+fi
+
+echo "Downloading files"
+mkdir /tmp/pwnd-download
+cd /tmp/pwnd-download
+wget -O pwn https://github.com/MnxCool/pwnd/raw/master/pwn
+wget -O pwnd https://raw.githubusercontent.com/MnxCool/pwnd/master/pwnd
+wget -O pwn-daemon https://raw.githubusercontent.com/MnxCool/pwnd/master/pwn-daemon
+wget -O https://raw.githubusercontent.com/MnxCool/pwnd/master/install.sh
+echo "Downloaded successful. Installing..."
+bash install.sh
+cd /
+rm -rf /tmp/pwnd-download
+exit 0
